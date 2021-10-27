10c206e3fb6ef849a81beb8b153a1a7f19ef726d
yuri-bukhalenkov <78025148+yuri-bukhalenkov@users.noreply.github.com>
2021-10-27 18:43:43 +0300

    ED-310/multi-scope (#8)
    * multiscope for LimitConfig & LimitConfigParams

--- proto/limiter_config.thrift
+++ proto/limiter_config.thrift
@@ -23,7 +23,7 @@ struct LimitConfigParams {
     5: required time_range.TimeRangeType time_range_type
     6: required LimitContextType context_type
     7: required LimitType type
-    8: required LimitScope scope
+    8: required set<LimitScope> scope
     9: optional string description
     10: required OperationLimitBehaviour op_behaviour
 }
@@ -38,9 +38,10 @@ struct LimitConfig {
     7: required time_range.TimeRangeType time_range_type
     11: required LimitContextType context_type
     8: optional LimitType type
-    9: optional LimitScope scope
+    9: optional LimitScope scope_deprecated
     10: optional string description
     12: optional OperationLimitBehaviour op_behaviour
+    13: optional set<LimitScope> scope
 }
 
 struct OperationLimitBehaviour {
