e81239e243b5eec49ff75749c9d0a0472a86d3fe
Артем <WWW_cool@inbox.ru>
2022-06-28 18:29:54 +0300

    TD-328: Add withdrawal context (#22)
    * added withdrawal context
    
    * renamed
    
    * added withdrawal config context type
    
    * renamed
    
    * renamed

---
+++ proto/limiter_withdrawal_context.thrift
@@ -0,0 +1,23 @@
+include "proto/withdrawals_domain.thrift"
+
+namespace java dev.vality.limiter.withdrawal.context
+namespace erlang limproto.context.withdrawal
+
+/**
+ * Контекст, получаемый из сервисов, реализующих один из интерфейсов протокола
+ * (например withdrawal в fistful)
+ */
+struct Context {
+    1: optional Operation op
+    2: optional Withdrawal withdrawal
+}
+
+union Operation {
+    1: OperationWithdrawal withdrawal
+}
+
+struct OperationWithdrawal {}
+
+struct Withdrawal {
+    1: optional withdrawals_domain.Withdrawal withdrawal
+}
