740292c9b46b9757f8465d6ec386f56f1f83c9a8
Артем <WWW_cool@inbox.ru>
2024-05-29 13:17:00 +0300

    added destination

--- proto/limiter_withdrawal_context.thrift
+++ proto/limiter_withdrawal_context.thrift
@@ -6,6 +6,9 @@ include "limiter_base.thrift"
 namespace java dev.vality.limiter.withdrawal.context
 namespace erlang limproto.context.withdrawal
 
+typedef string Token
+typedef string ID
+
 /**
  * Контекст, получаемый из сервисов, реализующих один из интерфейсов протокола
  * (например withdrawal в fistful)
@@ -25,4 +28,31 @@ struct Withdrawal {
     1: optional withdrawals_domain.Withdrawal withdrawal
     2: optional limiter_base.Route route
     3: optional base.ID wallet_id
+    4: optional Destination destination
+}
+
+struct Destination {
+    1: optional AuthData auth_data
+    2: optional Resource resource
+}
+
+union AuthData {
+    1: SenderReceiverAuthData sender_receiver
+}
+
+struct SenderReceiverAuthData {
+    1: optional Token sender
+    2: optional Token receiver
+}
+
+union Resource {
+    1: ResourceBankCard bank_card
+}
+
+struct ResourceBankCard {
+    1: optional BankCard bank_card
+}
+
+struct BankCard {
+    1: optional Token token
 }
