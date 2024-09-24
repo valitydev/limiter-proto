a21f05367d6d333def253604b966b5a49ddd606f
Артем <WWW_cool@inbox.ru>
2024-09-24 11:22:06 +0300

    added batches

--- proto/limiter.thrift
+++ proto/limiter.thrift
@@ -10,6 +10,7 @@ namespace erlang limproto.limiter
 
 typedef string LimitChangeID
 typedef string LimitID
+typedef string OperationID
 typedef base.ID PartyID
 typedef base.ID ShopID
 typedef base.ID WalletID
@@ -49,8 +50,14 @@ struct Limit {
 
 struct LimitChange {
     1: required LimitID id
-    2: required LimitChangeID change_id
+    // For single requests only
+    2: optional LimitChangeID change_id
     3: optional Version version
+}
+
+struct LimitRequest {
+    1: required OperationID operation_id
+    2: required list<LimitChange> limit_changes
 }
 
 exception LimitNotFound {}
@@ -98,6 +105,40 @@ service Limiter {
     )
 
     Clock Rollback(1: LimitChange change, 2: Clock clock, 3: LimitContext context) throws (
+        1: LimitNotFound e1,
+        2: LimitChangeNotFound e2,
+        3: base.InvalidRequest e3,
+        /*
+         * Исключения бизнес-логики повторяются для `Rollback` с целью
+         * согласованности контракта при вызове метода с тем же контекстом что
+         * и в методе `Hold`.
+         * То есть клиенту при последовательном обращении к методам удержания и
+         * отката с одними и теми же контекстом и идентификаторами изменений
+         * гарантируется что те же самые ошибки бизнес-логики будут явно
+         * сообщены, а операция отката не будет пытаться найти и изменить
+         * состояния не удержанного изменения.
+         */
+        4: InvalidOperationCurrency e4,
+        5: OperationContextNotSupported e5,
+        6: PaymentToolNotSupported e6
+    )
+
+    list<Limit> HoldBatch(1: LimitRequest request, 2: LimitContext context) throws (
+        1: LimitNotFound e1,
+        3: base.InvalidRequest e2,
+        4: InvalidOperationCurrency e3,
+        5: OperationContextNotSupported e4,
+        6: PaymentToolNotSupported e5
+    )
+
+    void CommitBatch(1: LimitRequest request, 2: LimitContext context) throws (
+        1: LimitNotFound e1,
+        2: LimitChangeNotFound e2,
+        3: base.InvalidRequest e3,
+        4: ForbiddenOperationAmount e4
+    )
+
+    Clock RollbackBatch(1: LimitRequest request, 2: LimitContext context) throws (
         1: LimitNotFound e1,
         2: LimitChangeNotFound e2,
         3: base.InvalidRequest e3,
